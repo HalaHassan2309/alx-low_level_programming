5035b9694cf5eeb1891c7a69c8ac8aa5236ebf93
HalaHassan2309 <hala.mohamed0924@gmail.com>
2023-02-21 09:55:11 -0800

    c

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _isalpha - return 1 char 'c' is letter
+ *
+ * Return: Always 0 (Succes)
+ */
+int _isalpha(int c)
+{
+        if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
+        {
+                return (1);
+        }
+        return (0);
+}
