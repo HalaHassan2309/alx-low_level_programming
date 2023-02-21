3a7f84beca0d45ee389a1eed8f87aa978c89a23f
HalaHassan2309 <123985522+HalaHassan2309@users.noreply.github.com>
2023-02-22 00:06:04 +0200

    Update 104-fibonacci.c

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -12,9 +12,9 @@ int main()
   int t1 = 1, t2 = 2;
   int nextTerm = t1 + t2;
 
-  printf("%d, %d, ", t1, t2);
+  printf("%lu, %lu, ", t1, t2);
   for (i = 3; i <= n; ++i) {
-    printf("%d, ", nextTerm);
+    printf("%lu, ", nextTerm);
     t1 = t2;
     t2 = nextTerm;
     nextTerm = t1 + t2;
