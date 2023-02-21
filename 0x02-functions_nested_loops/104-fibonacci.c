ed443679ea9fe6d34f297ebd7e62c0cfb73e56c5
HalaHassan2309 <123985522+HalaHassan2309@users.noreply.github.com>
2023-02-22 00:16:42 +0200

    Update 104-fibonacci.c

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -10,7 +10,7 @@ int main(void)
 	unsigned long n1 = 0, n2 = 1, n3;
 	unsigned long n1_h1, n1_h2, n2_h1, n2_h2;
 	unsigned long h1, h2;
-	
+
 	for (inc = 0; inc < 92; inc++)
 	{
 		n3 = n1 + n2;
@@ -33,8 +33,7 @@ int main(void)
 		}
 		printf("%lu%lu", h1, h2);
 		if (inc != 98)
-			printf(", ");
-		
+			printf(", ");		
 		n1_h1 = n2_h1;
 		n1_h2 = n2_h2;
 		n2_h1 = h1;
