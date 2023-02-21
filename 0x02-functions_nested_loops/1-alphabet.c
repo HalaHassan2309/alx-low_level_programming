45168f7412b86af957024218e819d782bbe32756
HalaHassan2309 <hala.mohamed0924@gmail.com>
2023-02-21 07:48:13 -0800

    c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,7 +1,9 @@
 #include "main.h"
+
 /**
- *add - a function that adds two integers and returns the result 
- *Return: alphabet
+ * print_alphabet - a function that prints the alphabet, in lowercase
+ *
+ * Return: Always 0 (Succes)
  */
 void print_alphabet(void)
 {
