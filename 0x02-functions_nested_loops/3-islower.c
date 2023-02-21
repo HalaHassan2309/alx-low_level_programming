75e5ce24b0b93ba944c158a3b613760282796805
HalaHassan2309 <hala.mohamed0924@gmail.com>
2023-02-21 09:36:31 -0800

    c

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _islower - a function that prints the alphabet ten times
+ *
+ * Return: Always 0 (Succes)
+ */
+int _islower(int c)
+{
+	if (c >= 'a' || c <= 'z')
+	{
+		return (1);
+	} else 
+	{
+		return (0);
+	}
+}
