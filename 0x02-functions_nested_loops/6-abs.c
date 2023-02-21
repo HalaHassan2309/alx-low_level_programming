728f433ce4116ff0981e3e6c8274571a660ce56a
HalaHassan2309 <hala.mohamed0924@gmail.com>
2023-02-21 11:07:21 -0800

    c

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _abs - return 1 char 'c' is letter
+ * @n : the input
+ * Return: Always 0 (Succes)
+ */
+int _abs(int n)
+{
+        if (n > 0)
+        {
+		return (n);
+        } else if (n < 0)
+                return (-1*n);
+	}
+        return (0);
+}
