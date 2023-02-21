e6495824f9c0ed824eea42e703b314b6f9ff797b
HalaHassan2309 <hala.mohamed0924@gmail.com>
2023-02-21 13:54:05 -0800

    c

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -6,15 +6,13 @@
 */
 void printFibonacciNumbers(int n)
 {
-    int f1 = 0, f2 = 1, i;
+    int f1 = 0, f2 = 1, i, next;
   
     if (n < 1)
         return;
     printf("%d ", f1);
     for (i = 1; i < n; i++) {
-        printf("%d ", f2);
-        int next;
-	
+        printf("%d ", f2);	
 	next = f1 + f2;
         f1 = f2;
         f2 = next;
