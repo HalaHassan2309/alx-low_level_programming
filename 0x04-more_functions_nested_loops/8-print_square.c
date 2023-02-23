d7227e689f1edb2ee50a1b2272499823fd130456
HalaHassan2309 <hala.mohamed0924@gmail.com>
2023-02-23 10:59:56 -0800

    C

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * print_square -  a function that draws a diagonal line on the terminal
+ * @n: input number of times '\' should be printed
+ * Return: a diagonal
+ */
+void print_square(int n)
+{
+	int co, sp;
+
+	if (n <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		for (co = 1; co <= n; co++)
+		{
+			for (sp = 1; sp < n; sp++)
+			{
+				_putchar('#');
+			}
+			_putchar('\n');
+		}
+	}
+}
