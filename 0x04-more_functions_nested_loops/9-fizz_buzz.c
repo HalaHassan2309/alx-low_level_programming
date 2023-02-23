1b193e1bdbf7da873acd9a4e01207f5671d90ff1
HalaHassan2309 <hala.mohamed0924@gmail.com>
2023-02-23 11:14:47 -0800

    C

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * main - a function that prints the numbers, from 0 to 9
+ *
+ * Return: 0
+ */
+int main ()
+{
+	int i;
+	for (i = 1; i<=100; i++)
+	{
+		if ((i % 15) == 0)
+			printf("FizzBuzz ");
+		else if ((i % 3) == 0)
+			printf("Fizz ");
+		else if ((i % 5) == 0)
+			printf("Buzz ");
+		else
+			printf("%d ", i);
+	}
+	return (0);
+}
