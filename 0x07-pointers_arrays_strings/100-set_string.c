12ec234b23d1e480bc0ee18ad48fcbc6dd67619b
HalaHassan2309 <hala.mohamed0924@gmail.com>
2023-03-07 04:45:57 -0800

    c

--- 0x07-pointers_arrays_strings/100-set_string.c
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -1,12 +1,13 @@
-#include "main.h"
+[200~#include "main.h"
 
 /**
- * set_string - sets pointer val to char
- * @s: pointer
- * @to: char
- */
+ *  * set_string - sets pointer val to char
+ *   * @s: pointer
+ *    * @to: char
+ *     */
 
 void set_string(char **s, char *to)
 {
-  *s = to;
+	  *s = to;
 }
+
