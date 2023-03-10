b1353a568365d0602c9b706a1a63b91130ced78b
HalaHassan2309 <hala.mohamed0924@gmail.com>
2023-03-10 00:23:16 -0800

    C

---
+++ 0x09-static_libraries/1-isdigit.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _isdigit - a function that checks for uppercase character
+ * @c: input
+ * Return: 1 if c is uppercase, 0 otherwise
+ */
+int _isdigit(int c)
+{
+	if ((c >= 48) && (c <= 57))
+		return (1);
+	else
+		return (0);
+}
---
+++ 0x09-static_libraries/6-abs.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * _abs - return 1 char 'c' is letter
+ * @n : the input
+ * Return: Always 0 (Succes)
+ */
+int _abs(int n)
+{
+	return (n * ((n > 0) - (n < 0)));
+}
