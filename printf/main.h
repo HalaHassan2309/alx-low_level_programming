2eae017a5981b9937cf8600049eac36d33eac789
HalaHassan2309 <halamohamed0924@gmail.com>
2023-03-27 19:27:21 +0200

    C

---
+++ printf/main.h
@@ -0,0 +1,13 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdarg.h>
+
+int _printf(const char *format, ...);
+char _putchar(char c);
+void print_alphabet(void);
+
+
+#endif /* MAIN_H */
