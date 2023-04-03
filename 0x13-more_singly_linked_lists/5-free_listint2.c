a547b5407780fd963011a84e6461a566890b23d3
HalaHassan2309 <123985522+HalaHassan2309@users.noreply.github.com>
2023-04-03 23:50:40 +0200

    Update 5-free_listint2.c

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -8,7 +8,7 @@
 void free_listint2(listint_t **head)
 {
 	listint_t *temp_node;
-	
+
 	if (!(*head))
 	{
 		return;
@@ -20,6 +20,6 @@ void free_listint2(listint_t **head)
 		free(*head);
 		*head = temp_node;
 	}
-	
+
 	(*head) = NULL;
 }
