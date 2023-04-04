78990b1431a7ece4715fa63b5bb67221a5df8a22
HalaHassan2309 <halamohamed0924@gmail.com>
2023-04-04 21:24:32 +0200

    C

---
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -0,0 +1,46 @@
+#include "lists.h"
+
+/**
+ * delete_nodeint_at_index - deletes the node at an index
+ * @head: pointer to the head of the list
+ * @index: index of the node to be added
+ *
+ * Return: the address of the node
+ */
+int delete_nodeint_at_index(listint_t **head, unsigned int index)
+{
+	listint_t *h = (*head);
+    listint_t *old = NULL;
+	listint_t *previous = NULL;
+	unsigned int i = 0, len = 0;
+
+    while (h != NULL)
+	{
+		len += 1;
+		h = h->next;
+	}
+
+	if ((index > len) || (!len))
+		return (-1);
+	while (head != NULL)
+	{
+		if (i == index)
+		{
+			old = *head;
+			if (i == 0)
+			{
+				*head = old->next;
+				free(old);
+				return (1);
+			}
+			previous->next = old->next;
+			free(old);
+			return (1);
+		}
+		else if ((i + 1) == index)
+			previous = *head;
+		head = &((*head)->next);
+		i++;
+	}
+	return (-1);
+}
---
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -0,0 +1,53 @@
+#include "lists.h"
+
+/**
+ * insert_nodeint_at_index - returns the nth node 
+ * @head: pointer to the head of the list
+ * @idx: index of the node to be added
+ * @n: content of the new node
+ *
+ * Return: the address of the node
+ */
+listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
+{
+	listint_t *h = (*head);
+    listint_t *new = NULL;
+	listint_t *previous = NULL;
+	unsigned int i = 0;
+    size_t count = 0;
+
+    while (h != NULL)
+	{
+		count += 1;
+		h = h->next;
+	}
+
+	new = malloc(sizeof(listint_t));
+	if (new == NULL || idx > count)
+	{
+		free(new);
+		return (NULL);
+	}
+	new->n = n;
+	new->next = NULL;
+	while (head != NULL)
+	{
+		if (i == idx)
+		{
+			if (i == 0)
+			{
+				new->next = *head;
+				*head = new;
+				return (new);
+			}
+			new->next = previous->next;
+			previous->next = new;
+			return (new);
+		}
+		else if ((i + 1) == idx)
+			previous = *head;
+		head = &((*head)->next);
+		i++;
+	}
+	return (NULL);
+}
