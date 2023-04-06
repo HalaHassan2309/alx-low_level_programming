723cbea68586018defa1c55b78b74d1ccb18fd45
HalaHassan2309 <halamohamed0924@gmail.com>
2023-04-06 21:28:38 +0200

    C

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,39 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * binary_to_uint - convert a binary number to a decimal
+ * @b: char string
+ *
+ * Return: converted decimal number or 0
+ */
+unsigned int binary_to_uint(const char *b)
+{
+	unsigned int sum = 0, power = 1;
+	int len = 0;
+
+	if (b == NULL)
+		return (0);
+
+	while(b[len])
+    {
+		if (b[len] != '1' && b[len] != '0')
+			return (0);
+		else
+			len++;
+    }
+
+	len--;
+
+	while (len >= 0)
+	{
+		if (b[len] == '1')
+		{
+			sum += power;
+		}
+		power *= 2;
+		len--;
+	}
+
+	return (sum);
+}
---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,32 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_binary - print binary representation of a number
+ * @n: decimal number to print as binary
+ */
+void print_binary(unsigned long int n)
+{
+	unsigned long int temp = n;
+	int num = 0;
+
+	if (n == 0)
+	{
+		printf("0");
+		return;
+	}
+
+	while ((temp >>= 1) > 0)
+	{
+		num++;
+	}
+
+	while (num >= 0)
+	{
+		if((n >> num) & 1)
+			printf("1");
+		else
+			printf("0");
+		num--;
+	}
+}
