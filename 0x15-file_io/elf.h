785e3f832f352d5c5751f05824f604e913fe760e
HalaHassan2309 <halamohamed0924@gmail.com>
2023-04-10 19:46:08 +0200

    C

--- 0x15-file_io/elf.h
+++ 0x15-file_io/elf.h
@@ -9,11 +9,15 @@
 #include <stdlib.h>
 #include <elf.h>
 
-void close_elf(int elf);
+void check_elf(unsigned char *e_ident);
+void print_magic(unsigned char *e_ident);
+void print_class(unsigned char *e_ident);
+void print_data(unsigned char *e_ident);
+void print_version(unsigned char *e_ident);
+void print_abi(unsigned char *e_ident);
+void print_osabi(unsigned char *e_ident);
 void print_type(unsigned int e_type, unsigned char *e_ident);
 void print_entry(unsigned long int e_entry, unsigned char *e_ident);
-void print_osabi(unsigned char *e_ident);
-void check_elf(unsigned char *e_ident);
-void print_version(unsigned char *e_ident);
+void close_elf(int elf);
 
 #endif /* ELF_H */
